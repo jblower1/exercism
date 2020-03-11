5fd159ddc549494156127ae4ec4694f62f753657
Joe Blower <jblower13@gmail.com>
2020-03-11 18:49:26 +0000

    Altered solution to import methods from previous resistor solution

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,6 +1,3 @@
+import { colorCode } from '../resistor-color/resistor-color.js' 
 
 export const decodedValue = (colors) => Number(colors.reduce((acc, color)=> acc + colorCode(color), "").substring(0,2)) 
-
-export const colorCode = (color) => COLORS.indexOf(color)
-
-export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
