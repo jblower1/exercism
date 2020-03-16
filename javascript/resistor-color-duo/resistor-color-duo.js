220016f4c73731bd51034a6c4c60c8c5165d72b9
Joe Blower <jblower13@gmail.com>
2020-03-16 23:34:21 +0000

    Updated resistor solution with array destructuring, eslint added for colltz

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,3 +1,3 @@
 import { colorCode } from '../resistor-color/resistor-color.js' 
 
-export const decodedValue = (colors) => Number(colors.reduce((acc, color)=> acc + colorCode(color), "").substring(0,2)) 
+export const decodedValue = ([first, second]) => Number(`${colorCode(first)}${colorCode(second)}`) 
